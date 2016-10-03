c581e98968326b36a83e9ac33ec952f9133b01bf
root <root@debian>
2016-10-03 00:57:20 -0400

    New Python Patterns

---
+++ python/prob1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Eric Manganaro
+
+# Python Project Euler #1
+
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we g$
+#Find the sum of all the multiples of 3 or 5 below 1000.
+
+
