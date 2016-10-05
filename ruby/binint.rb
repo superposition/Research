0e4920de19112ee5ed283de0112f0d00c61c2edb
superposition <ericmanganaro@gmail.com>
2016-10-05 02:03:41 -0400

    whatup

---
+++ ruby/binint.rb
@@ -0,0 +1,7 @@
+= begin
+
+Write a function that prints out the binary form of an int
+
+= end
+
+ARGF.read.to_s(2)
---
+++ ruby/dynamicprime.rb
@@ -0,0 +1,6 @@
+=begin
+
+Use dynamic programming to find the first X prime numbers
+
+=end
+
