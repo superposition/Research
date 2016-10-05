adbede64850fa19a370bc7d6cd82fa84d8778e3b
Eric M <eric@debian>
2016-10-04 21:47:26 -0400

    yesp

--- python/findpairs.py
+++ python/findpairs.py
@@ -3,5 +3,5 @@ list = [1, 1, 2, 3, 4, 5,]
 uniquelist = set(list)
 targetsum = 5
 for n in itertools.combinations(uniquelist, 2):
-    if n[0] + n[1] == targetsum:
-        print str(n[0]) + " + " + str(n[1])
+	if n[0] + n[1] == targetsum:
+       		print str(n[0]) + " + " + str(n[1])
