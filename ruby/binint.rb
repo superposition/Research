c1879d019c56c850655511ba16cdfad95135d51c
Eric M <eric@debian>
2016-10-05 02:22:53 -0400

    Square Root implementation

--- ruby/binint.rb
+++ ruby/binint.rb
@@ -1,7 +1,9 @@
-= begin
+#!usr/bin/ruby
+
+=begin
 
 Write a function that prints out the binary form of an int
 
-= end
+=end
 
-ARGF.read.to_s(2)
+p 9.to_s(2)
---
+++ ruby/sqrt.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+=begin
+implement the square root function
+=end
+
+class Float
+	def sqrt; Math.sqrt(self); end
+end
+
+
+p (9.234).sqrt
